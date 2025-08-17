5e9b138aa1ad12749888a1c70f997d720186a388
DDevNinja <patildurgesh4002@gmail.com>
2025-08-17 15:05:26 +0530

    Update stats.api.js

--- Frontend/src/api/stats.api.js
+++ Frontend/src/api/stats.api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3000/api/stats';
+const API_BASE_URL = 'https://urlshortener-xifk.onrender.com/api/stats';
 
 export const getUserCount = async () => {
     const response = await axios.get(`${API_BASE_URL}/users`);
